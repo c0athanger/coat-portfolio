83540dcaaa94da8aae7d1c417ad092c6c85cdc9b
c0athanger <jhill044@uottawa.ca>
2025-07-23 17:18:50 -0400

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={`${inter.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
